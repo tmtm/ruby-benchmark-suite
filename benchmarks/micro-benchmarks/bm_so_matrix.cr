8a88555024764adc82c5d3b613367f4d6bb84dab
TOMITA Masahiro <tommy@tmtm.org>
2015-11-05 00:35:17 +0900

    bm_so_matrix

--- benchmarks/micro-benchmarks/bm_so_matrix.cr
+++ benchmarks/micro-benchmarks/bm_so_matrix.cr
@@ -5,22 +5,22 @@ require "../../utils/bench"
 
 def mkmatrix(rows, cols)
     count = 1
-    mx = Array.new(rows)
+    mx = Array(Array(Int32)).new
     (0 .. (rows - 1)).each do |bi|
-        row = Array.new(cols, 0)
+        row = Array(Int32).new(cols, 0)
         (0 .. (cols - 1)).each do |j|
             row[j] = count
             count += 1
         end
-        mx[bi] = row
+        mx.push row
     end
     mx
 end
 
 def mmult(rows, cols, m1, m2)
-    m3 = Array.new(rows)
+    m3 = Array(Array(Int32)).new
     (0 .. (rows - 1)).each do |bi|
-        row = Array.new(cols, 0)
+        row = Array(Int32).new(cols, 0)
         (0 .. (cols - 1)).each do |j|
             val = 0
             (0 .. (cols - 1)).each do |k|
@@ -28,7 +28,7 @@ def mmult(rows, cols, m1, m2)
             end
             row[j] = val
         end
-        m3[bi] = row
+        m3.push row
     end
     m3
 end
@@ -38,7 +38,7 @@ size = 30
 Bench.run [60] do |n|
   m1 = mkmatrix(size, size)
   m2 = mkmatrix(size, size)
-  mm = Array.new
+  mm :: Array(Array(Int32))
   n.times do
       mm = mmult(size, size, m1, m2)
   end

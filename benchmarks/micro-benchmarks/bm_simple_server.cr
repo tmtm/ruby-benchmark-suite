f9b4616324128a88acaf4cee782262e713cd83e3
TOMITA Masahiro <tommy@tmtm.org>
2015-11-04 16:13:56 +0900

    bm_simple_server

--- benchmarks/micro-benchmarks/bm_simple_server.cr
+++ benchmarks/micro-benchmarks/bm_simple_server.cr
@@ -3,10 +3,10 @@ require "socket"
 
 Bench.run [1, 100, 100000] do |n|
   server = TCPServer.new "127.0.0.1", 0
-  client = TCPSocket.new server.addr[3], server.addr[1]
+  client = TCPSocket.new server.addr.ip_address.to_s, server.addr.ip_port.to_s.to_i
   server_conn = server.accept
   n.times do
-    client.write "a"
-    server_conn.recv 1024 # "a"
+    client.print "a"
+    server_conn.read_nonblock 1024 # "a"
   end
 end

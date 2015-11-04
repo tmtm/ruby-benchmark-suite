aa06db468092e5fcbe154f3280946737013bb0b2
TOMITA Masahiro <tommy@tmtm.org>
2015-11-04 16:05:12 +0900

    bm_simple_connect

--- benchmarks/micro-benchmarks/bm_simple_connect.cr
+++ benchmarks/micro-benchmarks/bm_simple_connect.cr
@@ -4,7 +4,7 @@ require "socket"
 
 Bench.run [1, 100, 500] do |n|
   server = TCPServer.new "127.0.0.1", 0
-  host, port = server.addr[3], server.addr[1]
+  host, port = server.addr.ip_address.to_s, server.addr.ip_port.to_s.to_i
 
   n.times do
    client = TCPSocket.new host, port
